ae72108a0a0211204f8a239c5ede6067fdeb7345
wcoding <papa5944@gmail.com>
2024-05-09 00:29:27 +0900

    give style to the app component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React , {useState, useEffect}from "react";
+import styles from './style/App.module.css';
+// import './style.css';
 
 function App() {
+
+  const [data, setData] = useState([])
+
+  const music = async () =>{
+    const url = 'https://spotify23.p.rapidapi.com/search/?q=%3CREQUIRED%3E&type=multi&offset=0&limit=10&numberOfTopResults=5';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '702e531af8mshe7690baa6521f69p19bfaejsn75b4ef9a7b5c',
+		'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.json();
+  // setData(result)
+  console.log(result)
+  // alert(JSON.stringify(result))
+ 
+} catch (error) {
+	console.error(error);
+}
+  }
+
+
+//   useEffect(()=>{
+//  music();
+//   }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className={styles.div}> 
+      <div className={styles.searchBox}>
+        <h1 className={styles.h1}>Jammming Spofity</h1>
+        <form>
+          <label htmlFor="searchMusic" className={styles.searchTitle}>Search Music</label><br />
+          <input type="search" name="search"  className={styles.searchForm}/>
+          <br/>
+          <br/>
+          <input type="submit" value='Search' className={styles.submit} />
+        </form>
+
+      </div>
+
+      <div className={styles.musicBox}>
+         <section className={styles.results}>
+          <p>result</p>
+         </section>
+         <section className={styles.save}>
+          <p>save</p>
+         </section>
+      </div>
+
+
+    
+    
     </div>
-  );
+  )
+  
 }
 
 export default App;
